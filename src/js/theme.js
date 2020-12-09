6d5bb062e852c6662c40acc23930217ec036bca2
Rogério Bonfim <rogeriomateus@icloud.com>
2020-12-09 19:56:05 -0300

    corecoes

--- src/js/theme.js
+++ src/js/theme.js
@@ -9,3 +9,37 @@ if (videos.length) {
         })
     })
 }
+
+if (window.matchMedia('(min-width: 400px)').matches) {
+    /* a viewport tem pelo menos 400 pixels de largura */
+} else {
+    /* a viewport menos que 992 pixels de largura */
+
+    const targetProd = document.querySelector('.produtos')
+
+    if (targetProd) {
+        targetProd.classList.add('swiper-container')
+
+        const wrapper = targetProd.querySelector('ul')
+
+        wrapper.classList.remove('row')
+
+        wrapper.classList.add('swiper-wrapper')
+
+        const childs = [...targetProd.querySelectorAll('li')]
+
+        if (childs) {
+            childs.forEach((element) => {
+                element.classList.remove('col-lg-3', 'col-xs-6', 'col-sm-3')
+                element.classList.add('swiper-slide')
+            })
+        }
+
+        var swiper = new Swiper('.produtos.swiper-container', {
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        })
+    }
+}
